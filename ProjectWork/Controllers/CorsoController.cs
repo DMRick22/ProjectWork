47de4250059da6d7464c931d196381e3b072ff18
Riccardo De Marzi <demarziriccardo22@outlook.it>
2022-10-05 18:25:50 +0200

    Prima implementazione HomeAdmin

---
+++ ProjectWork/Controllers/CorsoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utility;
+using ProjectWork.Models;
+
+namespace ProjectWork.Controllers
+{
+    public class CorsoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HomeAdmin()
+        {
+            return View(DaoCorso.GetInstance().Read());
+        }
+    }
+}
--- ProjectWork/Program.cs
+++ ProjectWork/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Corso}/{action=HomeAdmin}/{id?}");
 
 app.Run();
