075ef979c1a4bb421bf4fdedcf46bcca4301f50d
Riccardo De Marzi <demarziriccardo22@outlook.it>
2022-10-06 09:26:06 +0200

    Modifiche

--- ProjectWork/Program.cs
+++ ProjectWork/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Corso}/{action=HomeAdmin}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
